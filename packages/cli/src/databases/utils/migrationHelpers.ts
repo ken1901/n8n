Malmeu/n8ntest

refactor(core): Delete boilerplate code across migrations (no-changelog)

packages/cli/src/databases/utils/migrationHelpers.ts
@@ -5,6 +5,7 @@ import type { QueryRunner } from 'typeorm/query-runner/QueryRunner';
 import config from '@/config';
 import { getLogger } from '@/Logger';
 import { inTest } from '@/constants';
+import type { MigrationClass } from '@db/types';
 
 const PERSONALIZATION_SURVEY_FILENAME = 'personalizationSurvey.json';
 
@@ -60,6 +61,25 @@ export function logMigrationEnd(migrationName: string, disableLogging = inTest):
 	}, 100);
 }
 
+export const wrapMigration = (migration: MigrationClass) => {
+	const name = migration.name;
+	const dbType = config.getEnv('database.type');
+	const dbName = config.getEnv(`database.${dbType === 'mariadb' ? 'mysqldb' : dbType}.database`);
+	const tablePrefix = config.getEnv('database.tablePrefix');
+
+	const { up, down } = migration.prototype;
+	Object.assign(migration.prototype, {
+		up: async (queryRunner: QueryRunner) => {
+			logMigrationStart(name);
+			await up.call(this, { queryRunner, tablePrefix, dbType, dbName });
+			logMigrationEnd(name);
+		},
+		down: async (queryRunner: QueryRunner) => {
+			await down.call(this, { queryRunner, tablePrefix, dbType, dbName });
+		},
+	});
+};
+
 function batchQuery(query: string, limit: number, offset = 0): string {
 	return `
 			${query}
@@ -90,8 +110,6 @@ export async function runInBatches(
 		offset += limit;
 	} while (batchedQueryResults.length === limit);
 }
-
-export const getTablePrefix = () => config.getEnv('database.tablePrefix');
 
 export const escapeQuery = (
 	queryRunner: QueryRunner,

ken1901/n8n

packages/cli/src/databases/utils/migrationHelpers.ts
@@ -5,6 +5,9 @@ import type { QueryRunner } from 'typeorm/query-runner/QueryRunner';
 import config from '@/config';
 import { getLogger } from '@/Logger';
 import { inTest } from '@/constants';
+import type { MigrationClass } from '@db/types';
+
+const logger = getLogger();
 
 const PERSONALIZATION_SURVEY_FILENAME = 'personalizationSurvey.json';
 
@@ -42,10 +45,10 @@ export function logMigrationStart(migrationName: string, disableLogging = inTest
 	if (disableLogging) return;
 
 	if (!logFinishTimeout) {
-		getLogger().warn('Migrations in progress, please do NOT stop the process.');
+		logger.warn('Migrations in progress, please do NOT stop the process.');
 	}
 
-	getLogger().debug(`Starting migration ${migrationName}`);
+	logger.debug(`Starting migration ${migrationName}`);
 
 	clearTimeout(logFinishTimeout);
 }
@@ -53,12 +56,32 @@ export function logMigrationStart(migrationName: string, disableLogging = inTest
 export function logMigrationEnd(migrationName: string, disableLogging = inTest): void {
 	if (disableLogging) return;
 
-	getLogger().debug(`Finished migration ${migrationName}`);
+	logger.debug(`Finished migration ${migrationName}`);
 
 	logFinishTimeout = setTimeout(() => {
-		getLogger().warn('Migrations finished.');
+		logger.warn('Migrations finished.');
 	}, 100);
 }
+
+export const wrapMigration = (migration: MigrationClass) => {
+	const dbType = config.getEnv('database.type');
+	const dbName = config.getEnv(`database.${dbType === 'mariadb' ? 'mysqldb' : dbType}.database`);
+	const tablePrefix = config.getEnv('database.tablePrefix');
+	const migrationName = migration.name;
+	const context = { tablePrefix, dbType, dbName, migrationName };
+
+	const { up, down } = migration.prototype;
+	Object.assign(migration.prototype, {
+		up: async (queryRunner: QueryRunner) => {
+			logMigrationStart(migrationName);
+			await up.call(this, { queryRunner, ...context });
+			logMigrationEnd(migrationName);
+		},
+		down: async (queryRunner: QueryRunner) => {
+			await down.call(this, { queryRunner, ...context });
+		},
+	});
+};
 
 function batchQuery(query: string, limit: number, offset = 0): string {
 	return `
@@ -90,8 +113,6 @@ export async function runInBatches(
 		offset += limit;
 	} while (batchedQueryResults.length === limit);
 }
-
-export const getTablePrefix = () => config.getEnv('database.tablePrefix');
 
 export const escapeQuery = (
 	queryRunner: QueryRunner,